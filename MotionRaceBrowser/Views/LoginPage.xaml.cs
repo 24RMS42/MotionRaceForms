a70dde9387587c0e995e49003a00278a9b10926a
24RMS42 <omura.dai113@outlook.com>
2018-10-01 12:45:24 +0300

    Init

---
+++ MotionRaceBrowser/Views/LoginPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MotionRaceBrowser.Views
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+            Content.FindByName<Button>("loginButton").Clicked += OnLoginButtonClicked;
+        }
+
+        void OnLoginButtonClicked(object sender, EventArgs args)
+        {
+            //LoginAsync();
+        }
+    }
+}
