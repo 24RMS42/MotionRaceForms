d1adce93c8ab3b72fe2aa92b6381ec05263b5d67
24RMS42 <omura.dai113@outlook.com>
2018-10-05 15:27:48 +0300

    Fix android release

--- MotionRaceBrowser/Constant/Constants.cs
+++ MotionRaceBrowser/Constant/Constants.cs
@@ -5,12 +5,12 @@ namespace MotionRaceBrowser.Constant
     {
         public static string BASE = "https://app.motionsrace.com/applogin.aspx";
 
-        #if TARGET_ANDROID
-        public static string ApplicaitonId = "FF89F2F6-EECE-4E84-AB65-59BA8D1F33FD";
-        public static string ApplicaitonSecret = "BDB4063F-127E-4158-B521-E9469EB2C35E";
-        #else
+#if __IOS__
         public static string ApplicaitonId = "237C451B-D1AC-41AF-A8FB-C8480C8A6B05";
         public static string ApplicaitonSecret = "CD026EAD-C735-46FA-840E-98040E74CAC5";
-        #endif
+#else
+        public static string ApplicaitonId = "FF89F2F6-EECE-4E84-AB65-59BA8D1F33FD";
+        public static string ApplicaitonSecret = "BDB4063F-127E-4158-B521-E9469EB2C35E";
+#endif
     }
 }
