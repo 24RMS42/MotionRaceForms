0b2e0c9535b40bfa1d1e881f93eb75e1a6d35afa
24RMS42 <omura.dai113@outlook.com>
2019-03-15 05:37:49 +0300

    Add ResGront iOS

---
+++ ResGront.iOS/AppDelegate.cs
@@ -0,0 +1,30 @@
+﻿using Foundation;
+using MotionRaceBrowser;
+using Plugin.CrossPlatformTintedImage.iOS;
+using UIKit;
+
+namespace ResGront.iOS
+{
+    // The UIApplicationDelegate for the application. This class is responsible for launching the
+    // User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
+    [Register("AppDelegate")]
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            global::Xamarin.Forms.Forms.Init();
+
+            App.ScreenWidth = (int)UIScreen.MainScreen.Bounds.Width;
+            App.ScreenHeight = (int)UIScreen.MainScreen.Bounds.Height;
+
+            TintedImageRenderer.Init();
+            FFImageLoading.Forms.Platform.CachedImageRenderer.Init();
+            ZXing.Net.Mobile.Forms.iOS.Platform.Init();
+
+            LoadApplication(new App());
+
+            return base.FinishedLaunching(app, options);
+        }
+    }
+}
+
